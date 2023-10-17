ab68eb121751d05b4600d452cdd9dfb04761c728
Michal Startek <michal.startek@gmail.com>
2023-10-17 14:49:31 +0200

    Support for pre-AVX2 cpus

--- setup.py
+++ setup.py
@@ -3,6 +3,16 @@ from setuptools import setup, find_packages
 from glob import glob
 
 
+def get_polars():
+    try:
+        import cpufeature
+    except ModuleNotFoundError:
+        return "polars"
+    if cpufeature.CPUFeature["AVX2"]:
+        return "polars"
+    return "polars-lts-cpu"
+
+
 setup(
     name="mmapped_df",
     version="0.0.1",
@@ -11,6 +21,6 @@ setup(
     author_email="author@gmail.com",
     description="Memory-mapped, on-disk pandas df",
     packages=find_packages(),
-    install_requires=["numpy", "pandas", "pyarrow", "polars", "numba", "h5py"],
+    install_requires=["numpy", "pandas", "pyarrow", "numba", "h5py"] + [get_polars()],
     scripts=glob("tools/*.py"),
 )
