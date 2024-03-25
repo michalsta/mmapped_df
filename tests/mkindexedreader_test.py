97c0de0067b574a1722a9be3ba3d00582d81c799
Michał Startek <michal.startek@mimuw.edu.pl>
2024-03-25 17:53:44 +0100

    Reformat

--- tests/mkindexedreader_test.py
+++ tests/mkindexedreader_test.py
@@ -5,15 +5,19 @@ import os
 from tqdm import tqdm
 
 
-
 example_size = 1000000
+
 
 def gr():
     return np.random.randint(low=0, high=10000, size=example_size, dtype=np.uint64)
 
+
 def group_col():
-    X = np.random.choice([0,1,2,10], size=example_size, replace=True, p=[0.9, 0.05, 0.04, 0.01])
+    X = np.random.choice(
+        [0, 1, 2, 10], size=example_size, replace=True, p=[0.9, 0.05, 0.04, 0.01]
+    )
     return np.cumsum(X)
+
 
 df = pd.DataFrame({"group_column": group_col(), "aaa": gr(), "bb ba": gr()})
 """
