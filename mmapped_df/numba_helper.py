148ddb0e72ae59a32d093a8f7d269add4058b7cc
matteo <matteo.lacki@gmail.com>
2024-04-24 11:21:39 +0200

    removed fucking mzs

--- mmapped_df/numba_helper.py
+++ mmapped_df/numba_helper.py
@@ -18,35 +18,3 @@ def mkindex(indexed) -> tuple[npt.NDArray, npt.NDArray, np.uint32]:
     index[0] = 0
     np.cumsum(counts, out=index[1:])
     return index, counts, last
-
-
-# @numba.njit(boundscheck=True)
-# def mkindex(indexed):
-#     index = np.empty(shape=indexed[-1] + 2, dtype=np.uint64)
-#     idx = 0
-#     for ii in range(len(index) - 1):
-#         while (indexed[idx]) < ii:
-#             idx += 1
-#         index[ii] = idx
-#     index[-1] = len(indexed) + 1
-#     return index
-
-
-@numba.njit(parallel=True)
-def get_intensity_weighted_mean_mzs(
-    ClusterIDs,
-    index,
-    counts,
-    mzs,
-    intensities,
-):
-    means = np.zeros(shape=(len(ClusterIDs),), dtype=np.float64)
-    for i in numba.prange(len(ClusterIDs)):
-        ClusterID = ClusterIDs[i]
-        start = index[ClusterID]
-        count = counts[ClusterID]
-        means[i] = np.average(
-            mzs[start : start + count],
-            weights=intensities[start : start + count],
-        )
-    return means
