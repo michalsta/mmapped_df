aa32b63a581c76bd9dac85305f87f7f909e87ea1
Michal Startek <michal.startek@gmail.com>
2024-05-17 18:55:53 +0200

    Some fixes

--- mmapped_df/main.py
+++ mmapped_df/main.py
@@ -132,11 +132,12 @@ class DatasetWriter:
 
     def append(self, **kwargs):
         if self.files is None:
-            self._reset_schema(pd.DataFrame(kwargs, copy=False))
+            pddf_arg = {k:[v] for k, v in kwargs.items()}
+            self._reset_schema(pd.DataFrame([pddf_arg], copy=False))
         length = None
         for file, dtype, colname in zip(self.files, self.dtypes, self.colnames):
             dat = dtype.type(kwargs[colname])
-            file.write(dat.tobytes())
+            file.write(dat.to_bytes())
             length = len(kwargs[colname])
         self.length += length
 
