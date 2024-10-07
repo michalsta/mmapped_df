605307b6606383b1f609f3e221d6f366f876764a
Michał Startek <michal.startek@mimuw.edu.pl>
2024-10-07 23:53:08 +0200

    Make polars dep optional

--- setup.py
+++ setup.py
@@ -4,20 +4,6 @@ from glob import glob
 from setuptools import find_packages, setup
 
 
-def get_polars():
-    try:
-        import cpuinfo
-    except ModuleNotFoundError:
-        return "polars"
-    info = cpuinfo.get_cpu_info()
-    if info["arch"] != "X86_64":
-        return "polars"
-    if "avx2" in info["flags"]:
-        return "polars"
-    else:
-        return "polars-lts-cpu"
-
-
 setup(
     name="mmapped_df",
     version="0.0.1",
@@ -26,7 +12,7 @@ setup(
     author_email="author@gmail.com",
     description="Memory-mapped, on-disk pandas df",
     packages=find_packages(),
-    install_requires=["numpy", "pandas", "pyarrow", "numba", "h5py", "numba_progress"]
-    + [get_polars()],
+    install_requires=["numpy", "pandas", "pyarrow", "numba", "h5py", "numba_progress"],
+    extras_require={"polars": ["install-proper-polars"]},
     scripts=glob("tools/*.py"),
 )
