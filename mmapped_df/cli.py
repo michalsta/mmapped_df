3337401b12575969cb518c9104de4732f3a98de7
matteo <matteo.lacki@gmail.com>
2024-11-13 14:29:54 +0100

    added rename_column script

--- mmapped_df/cli.py
+++ mmapped_df/cli.py
@@ -3,6 +3,7 @@
 from pathlib import Path
 
 import click
+
 from mmapped_df.main import _read_schema_tbl, write_schema
 from mmapped_df.misc import shell
 
@@ -52,3 +53,45 @@ def copy_column(
 
     shell(f"cp --reflink=auto {old_file} {new_file}")
     write_schema(schema, path)
+
+
+# path = Path("/home/matteo/fragments.startrek")
+# old_name = "step"
+# new_name = "midia_cycle"
+# ignore_errors = False
+@click.command(context_settings={"show_default": True})
+@click.argument("path", type=Path)
+@click.argument("old_name", type=str)
+@click.argument("new_name", type=str)
+@click.option("--ignore_errors", is_flag=True)
+def rename_column(
+    path: Path,
+    old_name: str,
+    new_name: str,
+    ignore_errors: bool = False,
+) -> None:
+    """Copy a column in an existing memmapped data frame.
+
+    Remark: on CoW file systems this will not make any data copies.
+
+    Arguments:\n
+        path (pathlib.Path): Path to the memmapped folder.\n
+        old_name (str): Name of the column to copy.\n
+        naw_name (str): Name for the newly copied data.\n
+        ignore_errors (bool): Finish script successfully whatever assumption not met.\n
+    """
+    schema = _read_schema_tbl(path.expanduser())
+
+    if not old_name in schema:
+        if not ignore_errors:
+            raise KeyError(
+                f"Missig column {old_name} in the table. Present are: {list(schema)}."
+            )
+        return
+
+    assert (
+        not new_name in schema
+    ), f"Column `{old_name}` is already present in table saved under `{path}`."
+
+    new_schema = schema.rename(columns={old_name: new_name})
+    write_schema(new_schema, path)
